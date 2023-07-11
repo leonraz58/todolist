e24e66eed9d11bbe7addf848823551270acb2c89
leonraz58 <leonraz58@gmail.com>
2023-07-11 21:59:14 +0300

    08 user reduser immutability

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -11,14 +11,17 @@ type ActionType = {
 // меня вызовут и дадут мне стейт (почти всегда объект)
 // и инструкцию (action, тоже объект)
 // согласно прописанному type в этом action (инструкции) я поменяю state
-export const userReducer = (state: StateType, action: ActionType) => {
+export const userReducer = (state: StateType, action: ActionType):StateType => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1
-            return state
+            let newState = {...state}
+            newState.age = newState.age + 1
+            return newState
         case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1
-            return state
+            return {
+               ...state,
+               childrenCount: state.childrenCount+1
+            }
         default:
             throw new Error('I don\'t understand this type')
     }
