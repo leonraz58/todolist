53e2cb39de5553cee988b2b12d2c47fb432c0882
leonraz58 <leonraz58@gmail.com>
2023-07-11 22:54:26 +0300

    08 TDD test for user-reducer

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -17,3 +17,11 @@ test('user reducer should increment only childrenCount', () => {
     expect(endState.age).toBe(20)
     expect(endState.childrenCount).toBe(3)
 })
+
+test('user reducer should change name of user', () => {
+    const startState = {name: 'Dimych', age: 20, childrenCount: 2}
+    const newName = 'Viktor'
+    const endState = userReducer(startState, {type: 'CHANGE-NAME', newName: newName})
+
+    expect(endState.name).toBe(newName)
+})
--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -22,6 +22,11 @@ export const userReducer = (state: StateType, action: ActionType):StateType => {
                ...state,
                childrenCount: state.childrenCount+1
             }
+        case 'CHANGE-NAME':
+            return {
+                ...state,
+                name: action.newName
+            }
         default:
             throw new Error('I don\'t understand this type')
     }
