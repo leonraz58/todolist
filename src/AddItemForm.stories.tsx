50a4c9fb67b049667343d132352e0cbf29746c57
leonraz58 <leonraz58@gmail.com>
2023-08-15 22:54:15 +0300

    12 AddItemForm stories

---
+++ src/AddItemForm.stories.tsx
@@ -0,0 +1,12 @@
+import {AddItemForm} from "./AddItemForm";
+import {action} from '@storybook/addon-actions'
+
+export default {
+    title: "AddItemForm Component",
+    component: AddItemForm
+}
+
+const callback = action('Button Add was pressed inside the form')
+export const AddItemFormBaseExample = (props: any) => {
+    return <AddItemForm addItem={callback}/>
+}
