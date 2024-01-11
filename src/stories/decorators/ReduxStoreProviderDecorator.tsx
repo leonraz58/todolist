db62ab81c24080756d61edb41b2610550ebe950e
leonraz58 <leonraz58@gmail.com>
2024-01-11 15:30:35 +0300

    15 restore stories

---
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {v1} from 'uuid'
+import {AppRootStateType} from "../../state/store";
+import {tasksReducer} from "../../state/tasks-reducer";
+import {TaskPriorities, TaskStatuses} from "../../api/todolist-api";
+import {todolistsReducer} from "../../state/todolists-reducer";
+import {appReducer} from "../../state/app-reducer";
+import {thunk} from "redux-thunk";
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer
+})
+
+const initialGlobalState: AppRootStateType = {
+    todolists: [
+        {id: "todolistId1", title: "What to learn", filter: "all", addedDate: '', order: 0, entityStatus: 'idle'},
+        {id: "todolistId2", title: "What to buy", filter: "all", addedDate: '', order: 0, entityStatus: 'loading'}
+    ] ,
+    tasks: {
+        ["todolistId1"]: [
+            {id: v1(), title: "HTML&CSS", status: TaskStatuses.Completed, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: v1(), title: "JS", status: TaskStatuses.Completed, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
+        ],
+        ["todolistId2"]: [
+            {id: v1(), title: "Milk", status: TaskStatuses.Completed, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: v1(), title: "React Book", status: TaskStatuses.Completed, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
+        ]
+    },
+    app: {
+        error: null,
+        status: "idle"
+    }
+};
+
+export const storyBookStore = createStore(rootReducer, initialGlobalState, applyMiddleware(thunk));
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => (
+    <Provider
+        store={storyBookStore}>{storyFn()}
+    </Provider>)
